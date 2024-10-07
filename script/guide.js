961a48d3108ce855493a8df92e7ceeee3ba5c63a
festus-sulumeti <sulumetifestus@outlook.com>
2024-10-07 20:18:21 +0300

    the guided file path

---
+++ script/guide.js
@@ -0,0 +1,8 @@
+// Select the button by its ID
+const getStartedBtn = document.getElementById('getStartedBtn');
+
+// Add a click event listener to the button
+getStartedBtn.addEventListener('click', function () {
+    // Redirect to guide.html
+    window.location.href = './elements/guide.html'; // Change this to the correct path if needed
+});
